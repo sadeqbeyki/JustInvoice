dd45784191522a9d5ed63bf7f5d77f40d44c0688
mardibak <mardibaki@gmail.com>
2023-03-30 23:46:29 +0430

    edit product methode contract defined

--- JI.DomainContracts/Products/IProductRepository.cs
+++ JI.DomainContracts/Products/IProductRepository.cs
@@ -1,8 +1,13 @@
-﻿using JI.DomainContracts.Common;
+﻿using JI.ApplicationContracts.Invoice;
+using JI.Domain.InvoiceAgg;
+using JI.DomainContracts.Common;
 using JI.DomainContracts.Products;
 
 namespace JI.Domain.ProductAgg;
 public interface IProductRepository : IBaseRepository<long, Product>
 {
     List<ProductDto> GetProducts();
+
+    void DelEdit(ProductDto product);
+    void AddEdit(ProductDto product);
 }
